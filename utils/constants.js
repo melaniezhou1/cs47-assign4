d03cbb16d481d1ad8984cdbede8e15cb6be6ac06
melaniezhou1 <72285795+melaniezhou1@users.noreply.github.com>
2022-02-12 16:21:23 -0800

    Add files via upload

---
+++ utils/constants.js
@@ -0,0 +1,13 @@
+export const CLIENT_ID = "c251f13f5d744819ac28fc254e9ba30f";
+export const SCOPES = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+  "streaming",
+  "user-read-email",
+  "user-read-private"
+];
+export const REDIRECT_URI = "exp://10.29.165.26:19000";
+export const ALBUM_ID = "TODO: INSERT YOUR ALBUM ID HERE IF YOU ARE USING ONE";
